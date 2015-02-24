cc56c072e1fdf0815a9575e2507c1ae03aab6037
Thomas HERBETH <thomas.herbeth@telecomnancy.net>
2015-02-24 17:35:16 +0100

    [+] Parsage des catégories dbPedia.

--- TestColibri/src/dbpediaobjects/DBCategory.java
+++ TestColibri/src/dbpediaobjects/DBCategory.java
@@ -1,22 +1,28 @@
 package dbpediaobjects;
 
 public class DBCategory {
-	private String name = "";
-	private String uri = "";
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getUri() {
-		return uri;
-	}
-	
-	public void setUri(String uri) {
-		this.uri = uri;
-	}
+    private String name = "";
+    private String uri = "";
+
+    public DBCategory(String name, String uri) {
+        super();
+        this.name = name;
+        this.uri = uri;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
 }
--- TestColibri/src/serverlink/URLReader.java
+++ TestColibri/src/serverlink/URLReader.java
@@ -3,7 +3,6 @@ package serverlink;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
