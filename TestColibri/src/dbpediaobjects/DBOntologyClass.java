6ebdd77842a0e59f7481ec07b6c25bc03b2b8240
Thomas HERBETH <thomas.herbeth@telecomnancy.net>
2015-03-04 18:00:51 +0100

    [+] Algo YOLO pour la comparaison, sur les ontologies (non testé).

--- TestColibri/src/dbpediaobjects/DBOntologyClass.java
+++ TestColibri/src/dbpediaobjects/DBOntologyClass.java
@@ -3,40 +3,44 @@ package dbpediaobjects;
 import java.util.ArrayList;
 
 public class DBOntologyClass {
-	private String label = "";
-	private String uri = "";
-	private ArrayList<String> parents = new ArrayList<String>();
-	
-	public DBOntologyClass(String label, String uri) {
-		this.label = label;
-		this.uri = uri;
-	}
-	
-	public String getLabel() {
-		return label;
-	}
-	
-	public void setLabel(String label) {
-		this.label = label;
-	}
-	
-	public String getUri() {
-		return uri;
-	}
-	
-	public void setUri(String uri) {
-		this.uri = uri;
-	}
-	
-	public void addParent(String parent) {
-		parents.add(parent);
-	}
-	
-	public void setParents(ArrayList<String> parents) {
-		this.parents = parents;
-	}
-	
-	public int getParentsNumber() {
-		return parents.size();
-	}
+    private String label = "";
+    private String uri = "";
+    private ArrayList<String> parents = new ArrayList<String>();
+
+    public DBOntologyClass(String label, String uri) {
+        this.label = label;
+        this.uri = uri;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public void addParent(String parent) {
+        parents.add(parent);
+    }
+
+    public void setParents(ArrayList<String> parents) {
+        this.parents = parents;
+    }
+
+    public ArrayList<String> getParents() {
+        return parents;
+    }
+
+    public int getParentsNumber() {
+        return parents.size();
+    }
 }
--- TestColibri/src/main/Main.java
+++ TestColibri/src/main/Main.java
@@ -19,52 +19,76 @@ public class Main {
     // http://mappings.dbpedia.org/server/templatestatistics/fr/?template=Infobox_Ch%C3%A2teau
     // http://dbpedia.org/ontology/
 
-    public static void main(String[] args) throws ParseException, IOException
-	{
-		//Crawling DB categories
-		DBCategoriesCrawler dbCategoriesCrawler = new DBCategoriesCrawler();
-		dbCategoriesCrawler.computeParents();
+    public static void main(String[] args) throws ParseException, IOException {
+        // Crawling DB categories
+        DBCategoriesCrawler dbCategoriesCrawler = new DBCategoriesCrawler();
+        dbCategoriesCrawler.computeParents();
         HashMap<String, DBCategory> dbcategories = dbCategoriesCrawler.getDbcategories();
-		
-		//Crawling DB ontologies
-		DBOntologyClassesCrawler dbOntologyClasses = new DBOntologyClassesCrawler();
-		dbOntologyClasses.computeParents();
-		HashMap<String, DBOntologyClass> dbontologies = dbOntologyClasses.getDbontologies();
-		
-		// We create the lattice
-		PediaLattice lattice = new PediaLattice();
-		//lattice.deleteFirstIterationAttributes();
-		ArrayList<PediaConcept> lc = lattice.execIterator();
-                
-//                for(PediaConcept c : lc){
-//                    System.out.println("/***********Concept************/");
-//                    for(String obj : c.getListeObjets())
-//                        System.out.println("objet: "+obj);
-//                    System.out.println("\n");
-//                    
-//                    for(String cat : c.getCategories())                       
-//                        System.out.println("catégorie: "+cat);
-//                    System.out.println("\n");
-//                    
-//                    for(PediaConcept par : c.getParents()){
-//                        for(String op :  par.getListeObjets())
-//                            System.out.println("objet du parent: "+op);
-//                    }
-//                    System.out.println("/***********Fin concept*********/\n\n");                  
-//                }
-		
-		for(PediaConcept c : lc) {
-			ArrayList<String> unionCategoriesParent = c.unionCategoriesParent();
-			ArrayList<String> categories = c.getCategories();
-			
-			for(String cat : categories) {
-				if(unionCategoriesParent.contains(cat)) {
-				    
-				} else {
-				    
-				}
-			}
-		}
-		
-	}
+
+        // Crawling DB ontologies
+        DBOntologyClassesCrawler dbOntologyClasses = new DBOntologyClassesCrawler();
+        dbOntologyClasses.computeParents();
+        HashMap<String, DBOntologyClass> dbontologies = dbOntologyClasses.getDbontologies();
+
+        // We create the lattice
+        PediaLattice lattice = new PediaLattice();
+        // lattice.deleteFirstIterationAttributes();
+        ArrayList<PediaConcept> lc = lattice.execIterator();
+
+        // for(PediaConcept c : lc){
+        // System.out.println("/***********Concept************/");
+        // for(String obj : c.getListeObjets())
+        // System.out.println("objet: "+obj);
+        // System.out.println("\n");
+        //
+        // for(String cat : c.getCategories())
+        // System.out.println("catégorie: "+cat);
+        // System.out.println("\n");
+        //
+        // for(PediaConcept par : c.getParents()){
+        // for(String op : par.getListeObjets())
+        // System.out.println("objet du parent: "+op);
+        // }
+        // System.out.println("/***********Fin concept*********/\n\n");
+        // }
+
+        /* Pour chaque concept */
+        for (PediaConcept c : lc) {
+            /* Récupérer l'union des catégories des parents */
+            ArrayList<String> latticeOntologyParents = c.unionCategoriesParent();
+            ArrayList<String> latticeOntologies = c.getCategories();
+
+            /* Pour chaque catégorie du concept */
+            for (String latticeOntology : latticeOntologies) {
+                /* Regarder si la catégorie est incluse dans les catégories de l'union */
+                if (latticeOntologyParents.contains(latticeOntology)) {
+                    /* YOLO à partir d'ici ! */
+                    
+                    DBOntologyClass dbPediaOntology = dbontologies.get(latticeOntology);
+                    ArrayList<String> latticeOntologyParentsCopy = new ArrayList<String>(latticeOntologyParents);
+                    ArrayList<String> dbPediaOntologyParentsCopy = new ArrayList<String>(dbPediaOntology.getParents());
+                    
+                    latticeOntologyParentsCopy.removeAll(dbPediaOntologyParentsCopy);
+                    
+                    if (!latticeOntologyParentsCopy.isEmpty()) {
+                        System.out.println("Tada :) Ces parents sont dans la lattice mais pas sur dbPedia !");
+                        System.out.println(latticeOntologyParentsCopy);
+                    }
+                    
+                    latticeOntologyParentsCopy = new ArrayList<String>(latticeOntologyParents);
+                    dbPediaOntologyParentsCopy.removeAll(latticeOntologyParentsCopy);
+                    
+                    if (!dbPediaOntologyParentsCopy.isEmpty()) {
+                        System.out.println("Tada :) Ces parents sont dans dbPedia mais pas dans notre treillis !");
+                        System.out.println(dbPediaOntologyParentsCopy);
+                    }
+                    
+                    /* partie YOLO terminée ! */
+                } else {
+
+                }
+            }
+        }
+
+    }
 }
