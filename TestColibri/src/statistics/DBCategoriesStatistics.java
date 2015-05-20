7461b0ee5ab1953c4b9717d77f38583db3074cf2
Pierre Monnin <pier.monn@gmail.com>
2015-05-20 22:03:36 +0200

    [+] Cleaning of DBCategoriesStatistics

--- TestColibri/src/statistics/DBCategoriesStatistics.java
+++ TestColibri/src/statistics/DBCategoriesStatistics.java
@@ -1,6 +1,5 @@
 package statistics;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Set;
@@ -36,7 +35,6 @@ public class DBCategoriesStatistics {
 		
 		// Orphans number, direct subsumptions number,
 		Set<String> keys = this.categories.keySet();
-		ArrayList<String> orphans = new ArrayList<>(); 
 		for(String key : keys) {
 			// Children relationship creation
 			for(String parent : this.categories.get(key).getParents()) {
@@ -49,9 +47,9 @@ public class DBCategoriesStatistics {
 			// Orphans
 			if(this.categories.get(key).getParentsNumber() == 0) {
 				this.orphansNumber++;
-				orphans.add(key);
 				
 				// Compute depth
+				System.out.println("Orphan + 1");
 				int currentDepth = computeDepth(key);
 				if(currentDepth > this.depth)
 					this.depth = currentDepth;
@@ -59,14 +57,7 @@ public class DBCategoriesStatistics {
 			
 			// Direct subsumptions
 			this.directSubsumptions += this.categories.get(key).getParentsNumber();
-			
-			// Depth preparation
-			this.categories.get(key).setDepth(-1);
 		}
-		
-		// Compute inferred subsumptions
-//		System.out.println("Computing inferred subsumptions");
-//		computeInferredSubsumptions();
 	}
 	
 	public void displayStatistics() {
@@ -83,79 +74,41 @@ public class DBCategoriesStatistics {
 		LinkedList<String> stack = new LinkedList<String>();
 		
 		// Algorithm initialization
-		for(String yagoClass : this.categories.keySet()) {
-			this.categories.get(yagoClass).setDepth(-1);
+		System.out.println("Depth initialization");
+		for(String category : this.categories.keySet()) {
+			this.categories.get(category).setDepth(-1);
 		}
 		this.categories.get(key).setDepth(0);
 		stack.push(key);
 		
 		// Algorithm computation
+		System.out.println("Depth computation");
 		while(!stack.isEmpty()) {
 			String cat = stack.pollFirst();
 			DBCategory dbCat = this.categories.get(cat);
 			
 			if(dbCat != null) {
-				if(dbCat.getChildrenNumber() == 0 && dbCat.getDepth() > currentDepth) {
+				boolean childrenModified = false;
+				for(String child : dbCat.getChildren()) {
+					DBCategory childClass = this.categories.get(child);
+					
+					if(childClass != null && childClass.getDepth() == -1) {
+						childClass.setDepth(dbCat.getDepth() + 1);
+						stack.add(child);
+						childrenModified = true;
+					}
+				}
+				
+				if(!childrenModified && dbCat.getDepth() > currentDepth) {
 					currentDepth = dbCat.getDepth();
 				}
 				
-				else {
-					for(String child : dbCat.getChildren()) {
-						DBCategory childClass = this.categories.get(child);
-						
-						if(childClass != null && childClass.getDepth() == -1) {
-							childClass.setDepth(dbCat.getDepth() + 1);
-							stack.add(child);
-						}
-					}
-				}
 			}
+			
+			System.out.println(currentDepth);
 		}
 		
+		System.out.println("Depth end");
 		return currentDepth;
-	}
-	
-	private void computeInferredSubsumptions() {
-		Set<String> keys = this.categories.keySet();
-		
-		// Algorithm for each category
-		int i = 0;
-		for(String cat : keys) {
-			System.out.println("Inferred subsumptions : " + i + " / " + keys.size());
-			
-			// Algorithm initialization
-			for(String key : keys) {
-				this.categories.get(key).setInferredSubsumptionsToken(false);
-			}
-			
-			boolean done = false;
-			// We put the token to true on every reachable category 
-			while(!done) {
-				done = true;
-				
-				for(String key : keys) {
-					if(this.categories.get(key).getInferredSubsumptions()) {
-						for(String parent : this.categories.get(key).getParents()) {
-							DBCategory parentCat = this.categories.get(parent);
-							
-							if(parentCat != null) {
-								if(!parentCat.getInferredSubsumptions()) {
-									parentCat.setInferredSubsumptionsToken(true);
-									done = false;
-								}
-							}
-						}
-					}
-				}
-			}
-			
-			// Algorithm disarming
-			for(String key : keys) {
-				if(this.categories.get(key).getInferredSubsumptions())
-					this.inferredSubsumptions++;
-			}
-			this.inferredSubsumptions -= this.categories.get(cat).getParentsNumber();
-			i++;
-		}
 	}
 }
