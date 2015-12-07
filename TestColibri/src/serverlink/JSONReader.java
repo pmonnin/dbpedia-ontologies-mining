c37386795a93591a6aef24046604e5c01154bfac
Pierre Monnin <pier.monn@gmail.com>
2015-12-07 23:11:24 +0100

    [-] Removing deleted server address

--- TestColibri/src/serverlink/JSONReader.java
+++ TestColibri/src/serverlink/JSONReader.java
@@ -17,7 +17,6 @@ public final class JSONReader {
     /**
      * Server address
      */
-    public static final String SERVER_LOC = "http://sbc2015.telecomnancy.univ-lorraine.fr:10000/publi/query?output=json&query=";
     public static final String SERVER_CRAN = "http://localhost:8890/sparql/?default-graph-uri=http%3A%2F%2Fdbpedia.org&format=application%2Fsparql-results%2Bjson&debug=on&query=";
     private static ObjectMapper mapper = new ObjectMapper();
 
