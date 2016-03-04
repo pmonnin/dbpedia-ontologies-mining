6554a2638984e5e28287616f0980b66b75206cee
Pierre Monnin <pier.monn@gmail.com>
2016-03-05 00:07:11 +0100

    Removed unused prefix in Yago classes hierarchy query

--- DBPediaAnalyzer/src/dbpediaanalyzer/factories/HierarchiesFactory.java
+++ DBPediaAnalyzer/src/dbpediaanalyzer/factories/HierarchiesFactory.java
@@ -130,7 +130,6 @@ public class HierarchiesFactory {
 
         try {
             SparqlResponse response = (new ServerQuerier()).runQuery(
-                    "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
                     "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> " +
                     "PREFIX owl:<http://www.w3.org/2002/07/owl#> " +
                     "select distinct ?child ?parent where {" +
