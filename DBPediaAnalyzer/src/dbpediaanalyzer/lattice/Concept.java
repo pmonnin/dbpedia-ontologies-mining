9555bdae88023863c69ca7c4ca292e818c0bf6e7
Pierre Monnin <pier.monn@gmail.com>
2016-03-16 23:06:56 +0100

    Implemented colibri lattice traversal

--- DBPediaAnalyzer/src/dbpediaanalyzer/lattice/Concept.java
+++ DBPediaAnalyzer/src/dbpediaanalyzer/lattice/Concept.java
@@ -1,5 +1,7 @@
 package dbpediaanalyzer.lattice;
 
+import dbpediaanalyzer.dbpediaobject.Page;
+
 import java.util.ArrayList;
 
 /**
@@ -9,16 +11,38 @@ import java.util.ArrayList;
  *
  */
 public class Concept {
+    private ArrayList<Page> objects;
+    private ArrayList<String> attributes;
     private ArrayList<Concept> parents;
     private ArrayList<Concept> children;
 
     public Concept() {
+        this.objects = new ArrayList<>();
+        this.attributes = new ArrayList<>();
         this.parents = new ArrayList<>();
         this.children = new ArrayList<>();
     }
 
+    void addAttributes(String attribute) {
+        if(!this.attributes.contains(attribute)) {
+            this.attributes.add(attribute);
+        }
+    }
+
+    void addParent(Concept parent) {
+        if(!this.parents.contains(parent)) {
+            this.parents.add(parent);
+        }
+    }
+
     public ArrayList<Concept> getParents() {
         return new ArrayList<>(this.parents);
+    }
+
+    void addChild(Concept child) {
+        if(!this.children.contains(child)) {
+            this.children.add(child);
+        }
     }
 
     public ArrayList<Concept> getChildren() {
--- DBPediaAnalyzer/src/dbpediaanalyzer/lattice/Lattice.java
+++ DBPediaAnalyzer/src/dbpediaanalyzer/lattice/Lattice.java
@@ -1,6 +1,11 @@
 package dbpediaanalyzer.lattice;
 
+import colibri.lib.Edge;
+import colibri.lib.Traversal;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * TODO JAVADOC
@@ -9,8 +14,66 @@ import java.util.HashMap;
  *
  */
 public class Lattice {
+    private ArrayList<Concept> concepts;
+    private Concept top;
+    private Concept bottom;
 
     public Lattice(colibri.lib.Lattice colibriLattice) {
+        this.concepts = new ArrayList<>();
 
+        // Traversing colibri lattice and creating custom lattice
+        Iterator<Edge> latticeIterator = colibriLattice.edgeIterator(Traversal.BOTTOM_ATTRSIZE);
+        HashMap<colibri.lib.Concept, Concept> processed = new HashMap<>();
+        while(latticeIterator.hasNext()) {
+            Edge edge = latticeIterator.next();
+
+            colibri.lib.Concept colibriUpperConcept = edge.getUpper();
+            colibri.lib.Concept colibriLowerConcept = edge.getLower();
+
+            if(!processed.containsKey(colibriUpperConcept)) {
+                Concept c = createConceptFromColibriConcept(colibriUpperConcept);
+                processed.put(colibriUpperConcept, c);
+                this.concepts.add(c);
+            }
+
+            if(!processed.containsKey(colibriLowerConcept)) {
+                Concept c = createConceptFromColibriConcept(colibriLowerConcept);
+                processed.put(colibriLowerConcept, c);
+                this.concepts.add(c);
+            }
+
+            Concept upperConcept = processed.get(colibriUpperConcept);
+            Concept lowerConcept = processed.get(colibriLowerConcept);
+
+            upperConcept.addChild(lowerConcept);
+            lowerConcept.addParent(upperConcept);
+        }
+
+        // Top & bottom detection
+        for(Concept c : this.concepts) {
+            if(c.getParents().size() == 0) {
+                this.top = c;
+            }
+
+            if(c.getChildren().size() == 0) {
+                this.bottom = c;
+            }
+        }
+    }
+
+    private Concept createConceptFromColibriConcept(colibri.lib.Concept colibriConcept) {
+        Concept concept = new Concept();
+
+        // TODO under development
+
+        return concept;
+    }
+
+    public Concept getTop() {
+        return this.top;
+    }
+
+    public Concept getBottom() {
+        return this.bottom;
     }
 }
