d18fc25bdeaa8acc2920e47fe897fbff922e3c6f
Pierre Monnin <pier.monn@gmail.com>
2016-03-23 22:36:42 +0100

    Implemented basic saving for lattice concepts: page URI and attributes

--- DBPediaAnalyzer/src/dbpediaanalyzer/io/LatticeWriter.java
+++ DBPediaAnalyzer/src/dbpediaanalyzer/io/LatticeWriter.java
@@ -3,6 +3,8 @@ package dbpediaanalyzer.io;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import dbpediaanalyzer.dbpediaobject.Page;
+import dbpediaanalyzer.lattice.Concept;
 import dbpediaanalyzer.lattice.Lattice;
 import dbpediaanalyzer.statistic.LatticeStatistics;
 
@@ -21,11 +23,42 @@ public class LatticeWriter {
         try {
             JsonGenerator jsonGenerator = (new JsonFactory()).createGenerator(new File(fileName), JsonEncoding.UTF8);
 
+            // Start of JSON global object
             jsonGenerator.writeStartObject();
 
-            
+            // Concepts saving
+            jsonGenerator.writeArrayFieldStart("concepts");
+            for(Concept concept : lattice.getConcepts()) {
+                // Start of concept JSON object
+                jsonGenerator.writeStartObject();
 
+                // Objects saving
+                jsonGenerator.writeArrayFieldStart("objects");
+                for(Page page : concept.getObjects()) {
+                    jsonGenerator.writeString(page.getURI());
+                }
+                jsonGenerator.writeEndArray();
+
+                // Attributes saving
+                jsonGenerator.writeArrayFieldStart("attributes");
+                for(String attribute : concept.getAttributes()) {
+                    jsonGenerator.writeString(attribute);
+                }
+                jsonGenerator.writeEndArray();
+
+                // End of concept object
+                jsonGenerator.writeEndObject();
+            }
+            jsonGenerator.writeEndArray();
+
+            // Edges saving
+            jsonGenerator.writeArrayFieldStart("edges");
+            jsonGenerator.writeEndArray();
+
+            // End of global JSON object
             jsonGenerator.writeEndObject();
+
+            jsonGenerator.close();
         }
 
         catch(IOException e) {
