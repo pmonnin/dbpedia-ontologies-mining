e609b68299b50bfd1c3e02aadf02359b015b6be0
Pierre Monnin <pier.monn@gmail.com>
2016-04-07 20:53:44 +0200

    Implemented comparison results statistics

--- DBPediaAnalyzer/src/dbpediaanalyzer/main/LatticeAnalysis.java
+++ DBPediaAnalyzer/src/dbpediaanalyzer/main/LatticeAnalysis.java
@@ -8,6 +8,7 @@ import dbpediaanalyzer.extraction.LatticeKnowledgeExtractor;
 import dbpediaanalyzer.factory.HierarchiesFactory;
 import dbpediaanalyzer.io.LatticeReader;
 import dbpediaanalyzer.lattice.Lattice;
+import dbpediaanalyzer.statistic.ComparisonResultsStatistics;
 import dbpediaanalyzer.util.TimeMeasurer;
 
 import java.util.List;
@@ -24,13 +25,15 @@ public class LatticeAnalysis {
      * TODO JAVADOC
      * @param args should contain
      *             - name of JSON file corresponding to the lattice to analyze
-     *             - name of output file where results will be written
+     *             - name of output file where comparison results will be written
+     *             - name of output file where statistics of comparison results will be written
      */
     public static void main(String[] args) {
-        if(args.length != 2) {
+        if(args.length != 3) {
             System.out.println("Usage:\n java LatticeAnalysis lattice output");
             System.out.println("\t lattice\n\t\t JSON file corresponding to the lattice to analyze");
-            System.out.println("\t output\n\t\t file where comparison results will be written");
+            System.out.println("\t output\n\t\t file where comparison results will be written (CSV format)");
+            System.out.println("\t comparison-statistics\n\t\t file where statistics of comparison results will be written");
         }
 
         else {
@@ -49,7 +52,12 @@ public class LatticeAnalysis {
             DataBasedKnowledgeManager dbkm = (new LatticeKnowledgeExtractor()).analyze(lattice);
             System.out.println("\t Computing comparison results...");
             List<ComparisonResult> comparisonResults = (new KnowledgesComparator()).compareKnowledges(dbkm);
-            System.out.println("\t Writing results inside output file...");
+            System.out.println("\t Computing comparison statistics...");
+            ComparisonResultsStatistics statistics = new ComparisonResultsStatistics(comparisonResults);
+
+            System.out.println("Saving results...");
+            System.out.println("\t Saving comparison results...");
+            System.out.println("\t Saving comparison results statistics...");
 
             tm.stop();
             System.out.println("Processing time: " + tm.toString());
---
+++ DBPediaAnalyzer/src/dbpediaanalyzer/statistic/ComparisonResultsStatistics.java
@@ -0,0 +1,55 @@
+package dbpediaanalyzer.statistic;
+
+import dbpediaanalyzer.comparison.ComparisonResult;
+import dbpediaanalyzer.comparison.ConfirmedDirectRelationship;
+import dbpediaanalyzer.comparison.ProposedInferredToDirectRelationship;
+import dbpediaanalyzer.comparison.ProposedNewRelationship;
+
+import java.util.List;
+
+/**
+ * TODO JAVADOC
+ *
+ * @author Pierre Monnin
+ *
+ */
+public class ComparisonResultsStatistics {
+    private int resultsNumber;
+    private int confirmedDirectRelationshipNumber;
+    private int proposedInferredToDirectRelationshipNumber;
+    private int proposedNewRelationshipNumber;
+
+    public ComparisonResultsStatistics(List<ComparisonResult> comparisonResults) {
+        this.resultsNumber = comparisonResults.size();
+
+        this.confirmedDirectRelationshipNumber = 0;
+        this.proposedInferredToDirectRelationshipNumber = 0;
+        this.proposedNewRelationshipNumber = 0;
+        for(ComparisonResult cr : comparisonResults) {
+            if(cr instanceof ConfirmedDirectRelationship) {
+                this.confirmedDirectRelationshipNumber++;
+            }
+
+            else if(cr instanceof ProposedInferredToDirectRelationship) {
+                this.proposedInferredToDirectRelationshipNumber++;
+            }
+
+            else if(cr instanceof ProposedNewRelationship) {
+                this.proposedNewRelationshipNumber++;
+            }
+        }
+
+    }
+
+    public int getResultsNumber() {
+        return this.resultsNumber;
+    }
+
+    public int getConfirmedDirectRelationshipNumber() {
+        return this.confirmedDirectRelationshipNumber;
+    }
+
+    public int getProposedNewRelationshipNumber() {
+        return this.proposedNewRelationshipNumber;
+    }
+}
