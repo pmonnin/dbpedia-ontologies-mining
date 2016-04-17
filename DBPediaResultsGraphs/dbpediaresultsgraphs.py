849cf0b3e36d7856ecd2162e258022accbeab48c
Pierre Monnin <pier.monn@gmail.com>
2016-04-18 00:11:13 +0200

    Improved statistics histogram by taking into account evaluation strategy

--- DBPediaResultsGraphs/dbpediaresultsgraphs.py
+++ DBPediaResultsGraphs/dbpediaresultsgraphs.py
@@ -4,24 +4,36 @@ import sys
 import csv
 import matplotlib.pyplot
 
+possible_strategies = ["NumberOfSubmissions", "AverageExtensionsRatio", "DistanceFromLCA"]
+
 
 def print_usage():
-    print("Usage:\n python dbpediaresultsgraphs.py comparison-results hist-confirmed hist-proposed-inferred-to-direct "
-          "hist-proposed-new")
+    print("Usage:\n python dbpediaresultsgraphs.py comparison-results hist-confirmed strategy-confirmed "
+          "hist-proposed-inferred-to-direct strategy-proposed-inferred-to-direct "
+          "hist-proposed-new strategy-proposed-new")
     print("\t comparison-results\n\t\t CSV file produced by LatticeAnalysis program to be analyzed")
     print("\t hist-confirmed\n\t\t histogram of values of confirmed relationships")
-    print("\t hist-proposed-inferred-to-direct\n\t\t histogram of values of values proposed to be changed from inferred"
-          " to direct")
+    print("\t strategy-confirmed\n\t\t strategy used during analysis to evaluate confirmed relationships")
+    print("\t hist-proposed-inferred-to-direct\n\t\t histogram of values of relationships proposed to be changed from "
+          "inferred to direct")
+    print("\t strategy-proposed-inferred-to-direct\n\t\t strategy used during analysis to evaluate relationships "
+          "proposed to be changed from inferred to direct")
     print("\t hist-proposed-new\n\t\t histogram of values of proposed new relationships")
+    print("\t strategy-proposed-new\n\t\t strategy used during analysis to evaluate proposed new relationships")
+    print("Possible values for strategies: NumberOfSubmissions, AverageExtensionsRatio, DistanceFromLCA")
+    print("If there are 0 values for a relationship type, related histogram is not produced")
 
 
 def check_command_arguments():
-    if len(sys.argv) != 5:
-        print_usage()
-        return False
+    correct_arguments = len(sys.argv) == 8 and \
+                        sys.argv[3] in possible_strategies and \
+                        sys.argv[5] in possible_strategies and \
+                        sys.argv[7] in possible_strategies
 
-    else:
-        return True
+    if not correct_arguments:
+        print_usage()
+
+    return correct_arguments
 
 
 def read_values_from_csv(csv_file):
@@ -33,11 +45,11 @@ def read_values_from_csv(csv_file):
     new = []
     for line in csvreader:
         if line[0] == "CONFIRMED_DIRECT":
-            confirmed.append(int(line[3]))
+            confirmed.append(float(line[3]))
         elif line[0] == "PROPOSED_INFERRED_TO_DIRECT":
-            inferred.append(int(line[3]))
+            inferred.append(float(line[3]))
         elif line[0] == "PROPOSED_NEW":
-            new.append(int(line[3]))
+            new.append(float(line[3]))
 
     fp.close()
     return confirmed, inferred, new
@@ -53,13 +65,27 @@ def plot_histogram_to_file(values, bins, title, histogram_file):
     matplotlib.pyplot.clf()
 
 
+def strategy_values_to_hist_bins(strategy, values):
+    if strategy == possible_strategies[0]:
+        return range(1, int(max(values)) + 2)
+
+    return 100
+
+
 if check_command_arguments():
     values_confirmed, values_inferred, values_new = read_values_from_csv(sys.argv[1])
 
-    if max(values_confirmed) == 0:
-        plot_histogram_to_file(values_confirmed, 1, "Values of confirmed relationships", sys.argv[2])
-    else:
-        plot_histogram_to_file(values_confirmed, max(values_confirmed), "Values of confirmed relationships", sys.argv[2])
+    # Plotting confirmed relationships histogram
+    if len(values_confirmed) != 0:
+        plot_histogram_to_file(values_confirmed, strategy_values_to_hist_bins(sys.argv[3], values_confirmed),
+                               "Values of confirmed relationships", sys.argv[2])
 
-    plot_histogram_to_file(values_inferred, 100, "Values of relationships to be changed from inferred to direct", sys.argv[3])
-    plot_histogram_to_file(values_new, 100, "Values of new relationships", sys.argv[4])
+    # Plotting relationships changed from inferred to direct histogram
+    if len(values_inferred) != 0:
+        plot_histogram_to_file(values_inferred, strategy_values_to_hist_bins(sys.argv[5], values_inferred),
+                               "Values of relationships to be changed from inferred to direct", sys.argv[4])
+
+    # Plotting new relationships histogram
+    if len(values_new) != 0:
+        plot_histogram_to_file(values_new, strategy_values_to_hist_bins(sys.argv[7], values_new),
+                               "Values of new relationships", sys.argv[6])
