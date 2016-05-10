75d977490793d8c2acdb1b8652295144c6a625b5
Pierre Monnin <pier.monn@gmail.com>
2016-05-10 20:13:15 +0200

    Minor output change

--- DBPediaAnalyzer/src/dbpediaanalyzer/main/DataInfo.java
+++ DBPediaAnalyzer/src/dbpediaanalyzer/main/DataInfo.java
@@ -144,6 +144,8 @@ public class DataInfo {
                         System.out.print(" -> ");
                     }
                 }
+
+                System.out.print("\n");
             }
         }
     }
